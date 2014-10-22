27cf548a9048a5dbb77c8ca3c5b33046b08fa1f3
xuhao <hao.xu@cs2c.com.cn>
2014-10-22 17:19:08 +0800

    update

--- src/soot/jimple/instrumenter/utils/FlowPathParser.java
+++ src/soot/jimple/instrumenter/utils/FlowPathParser.java
@@ -1,5 +1,20 @@
 package soot.jimple.instrumenter.utils;
 
-public class FlowPathParser {
+import soot.Unit;
+import soot.util.Chain;
 
+public class FlowPathParser {
+	private Chain<Unit> units;
+	
+	public FlowPathParser(String file) {
+		
+	}
+	
+	public FlowPathParser() {
+		
+	}
+	
+	public void parse() {
+		
+	}
 }
